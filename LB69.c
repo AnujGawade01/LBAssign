9b82090b762676ee093b28506468001ec82d190a
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-06-04 00:09:28 +0530

    Add files via upload

---
+++ LB69.c
@@ -0,0 +1,51 @@
+/* 4. Accept N number from user and return frequency of 11 from it */
+#include<stdio.h>
+#include<stdlib.h>
+
+int Frequency(int Arr[], int iLength)
+{
+    int iFrequency = 0;
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt<iLength; iCnt++)
+    {
+        if(Arr[iCnt] == 11)
+        {
+            iFrequency = iFrequency + 1;
+        }
+    }
+    return iFrequency;
+}
+
+int main()
+{
+    int iSize  = 0, iRet = 0, iCnt = 0;
+    int *p = NULL;
+
+    printf("Enter number of elements : \n");
+    scanf("%d",&iSize);
+
+    p = (int *) malloc (iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate memory");
+        return -1;
+    }
+
+    printf("Enter %d elements \n", iSize);
+
+    for( iCnt=0 ; iCnt<iSize ; iCnt++ )
+    {
+        printf("Enter element : %d\n",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Frequency(p, iSize);
+
+    printf("Frequency is %d", iRet);
+
+    free(p);
+    
+    return 0;
+}
