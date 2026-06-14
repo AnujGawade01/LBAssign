f962c62c0a18c966eae9de02b356b3a0ae7a19e4
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-06-14 15:01:51 +0530

    Add files via upload

---
+++ LB125.c
@@ -0,0 +1,50 @@
+// LB Assignment 25
+
+/*///////////////////////////////////////////////////////////////////////////////////////
+
+ 5. Accept number of Rows and number of columns from user and display below pattern
+
+    Input : iRow = 4 iCol = 4
+
+    Output :  1 2 3 4  
+              2 3 4 5 
+              3 4 5 6
+              4 5 6 7
+    
+*////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int iCntRow = 0, iCntCol = 0;
+
+
+    for(iCntRow = 1; iCntRow <= iRow; iCntRow++)
+    {
+        for(iCntCol = 1; iCntCol <= iCol; iCntCol++)
+        {   
+            if(iCntRow % 2 != 0)
+            {
+                printf("%d\t",iCntCol);
+            }
+            else
+            {
+                printf("%d\t",-iCntCol);
+            }
+        }
+        printf("\n");   
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of rows & columns: \n");
+    scanf("%d %d",&iValue1, &iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
