4cc9cf08af6ce3d4e11b3c7e7378ab296b3edc4d
AnujGawade01 <121755207+AnujGawade01@users.noreply.github.com>
2026-06-22 11:39:50 +0530

    Add files via upload

---
+++ LB167.c
@@ -0,0 +1,38 @@
+// LB Assignment 34
+
+/*///////////////////////////////////////////////////////////////////////////////////////
+
+ 2. Write a program which accept string from user and copy the contents of that string 
+ into another string. (Implement strncpy() function)
+
+ Note : if third parameter is greater than the size of sourcing string then copy the whole 
+ string
+
+*////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void StrNCpyX(char *src, char *dest, int iCnt)        
+{
+    while((*src != '\0') && (iCnt != 0))
+    {
+        *dest = *src;
+        src++;
+        dest++;
+        iCnt--;
+    }
+
+    *dest = '\0';
+}
+
+int main()
+{
+    char arr[30] = "Marvellous Multi OS";
+    char brr[30];   // Empty string
+
+    StrNCpyX(arr,brr,10);
+
+    printf("%s",brr);       // Marvellous
+
+    return 0;
+}
