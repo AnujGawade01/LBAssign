2fa664b436c3ef70f7f455c75b6b26a5f366bd10
Anuj Vinayak Gawade <121755207+AnujGawade01@users.noreply.github.com>
2026-07-24 00:00:33 +0530

    Add files via upload

---
+++ LB220.c
@@ -0,0 +1,35 @@
+// LB Assignment 44
+
+/*///////////////////////////////////////////////////////////////////////////////////////
+
+ 4. Write a recursive program which display below pattern
+ Input : 6
+ Output : a b c d e f
+
+/*///////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    static char ch = 'a';
+    if(0 != iNo)
+    {
+        printf("%c\t",ch);
+        ch++;
+        iNo--;
+        Display(iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter number : \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
